05280a3d0237fd297da0c9998b2dd865ca9e25e8
Pascal <pnieters@uos.de>
2020-03-26 12:44:37 +0100

    only includes covid19

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 import itertools as it
 
-diseases = ["campylobacter", "rotavirus", "borreliosis"]
-prediction_regions = ["germany", "bavaria"]
+diseases = ["covid19"]
+prediction_regions = ["germany"]
 
 combinations_age_eastwest = [(False,False),(False,True),(True,True)]
 combinations = list(it.product(range(len(combinations_age_eastwest)), diseases))
