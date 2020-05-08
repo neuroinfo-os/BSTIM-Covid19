57e22a8a806fad7b19fe859e00c7108010dab482
Jens Henrik Göbbert <j.goebbert@fz-juelich.de>
2020-05-08 13:38:47 +0200

    use preprocess_covid19_table.py in workflow notebook

--- src/preprocess_covid19_table.py
+++ src/preprocess_covid19_table.py
@@ -17,19 +17,25 @@ if __name__ == "__main__":
         "--source",
         nargs=1,
         dest="input_csv",
-        default="../data/raw/COVID19.csv",
+        default=["../data/raw/COVID19.csv"],
         help="provide the source csv table")
     parser.add_argument(
         "--destination",
         nargs=1,
         dest="output_csv",
-        default="../data/diseases/covid19.csv",
+        default=["../data/diseases/covid19.csv"],
         help="provide the destination file")
-
+    parser.add_argument(
+        "--shapes",
+        nargs=1,
+        dest="shapes_csv",
+        default=["../data/raw/germany_county_shapes.json"],
+        help="provide the shapes file for germany")
+    
     args = parser.parse_args()
 
     counties = OrderedDict()
-    with open("../data/raw/germany_county_shapes.json", "r") as data_file:
+    with open(args.shapes_csv[0], "r") as data_file:
         shape_data = json.load(data_file)
 
     for idx, val in enumerate(shape_data["features"]):
@@ -38,10 +44,11 @@ if __name__ == "__main__":
 
         counties[name_current] = id_current
 
-    covid19_data = pd.read_csv(args.input_csv, sep=',')
+    covid19_data = pd.read_csv(args.input_csv[0], sep=',')
 
     # this complicated procedure removes timezone information.
-    regex = re.compile(r"([0-9]+)-([0-9]+)-([0-9]+)T.*")
+    regex = re.compile(r"([0-9]{4})\/([0-9]{2})\/([0-9]{2}).*")
+    #regex = re.compile(r"([0-9]+)/([0-9]+)/([0-9]+).*")
     start_year, start_month, start_day = regex.search(
         covid19_data['Meldedatum'].min()).groups()
     end_year, end_month, end_day = regex.search(
@@ -53,6 +60,7 @@ if __name__ == "__main__":
     dates = [day for day in pd.date_range(start_date, end_date)]
     df = pd.DataFrame(index=dates)
     for county_name in counties:
+        print('.',end='')
         series = np.zeros(len(df), dtype=np.int32)
         lk_data = covid19_data[covid19_data['Landkreis'] == county_name]
         for (d_id, day) in enumerate(dates):
@@ -64,4 +72,4 @@ if __name__ == "__main__":
                 series[d_id] = cases
         df.insert(len(df.columns), counties[county_name], series)
 
-    df.to_csv(args.output_csv, sep=",")
+    df.to_csv(args.output_csv[0], sep=",")
