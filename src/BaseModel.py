72ab3e80b6a243b365b6f153fed6fd70399428de
lukeeffenberger <leffenberger@uos.de>
2020-06-10 12:18:33 +0200

    home

--- src/BaseModel.py
+++ src/BaseModel.py
@@ -453,26 +453,26 @@ class BaseModel(object):
             print("T_S",T_S.shape) # 12772, 5
             T_S = np.reshape(T_S, newshape=(31,412,-1))
             mean = np.mean(T_S, axis=0, keepdims=True)
-            T_S = np.reshape(np.tile(mean, reps=(31,1,1)), (-1,5))          
+            T_S = (1/31.0)*np.reshape(np.tile(mean, reps=(31,1,1)), (-1,5))          
             print("T_S",T_S.shape) # 12772, 5
  
             print("TS", TS.shape) 
             TS = np.reshape(TS, newshape=(31,412,-1))
             mean = np.mean(TS, axis=0, keepdims=True)
-            TS = np.reshape(np.tile(mean, reps=(31,1,1)),(-1,3)) 
+            TS = (1/31.0)*np.reshape(np.tile(mean, reps=(31,1,1)),(-1,3)) 
             print("TS", TS.shape) 
 
             print("T_D",T_D.shape) #12772, 0
             print(T_D)
             T_D = np.reshape(T_D, newshape=(31,412,-1))
             mean = np.mean(T_D, axis=0, keepdims=True)
-            T_D = np.reshape(np.tile(mean, reps=(31,1,1)), (-1)) 
+            T_D = (1/31.0)*np.reshape(np.tile(mean, reps=(31,1,1)), (-1)) 
             print("T_D",T_D.shape) #12772, 0
 
             print(log_exposure.shape) # 12272
             log_exposure = np.reshape(log_exposure, newshape=(31,412))
             mean = np.mean(log_exposure, axis=0, keepdims=True)
-            log_exposure = np.reshape(np.tile(mean, reps=(31,1)), (-1))
+            log_exposure = (1/31.0)*np.reshape(np.tile(mean, reps=(31,1)), (-1))
             print(log_exposure.shape) # 12272
           
             #mean = np.mean(log_exposure, axis=0)
@@ -521,8 +521,10 @@ class BaseModel(object):
                 IA_ef = np.dot(
                     ia_l.samples[np.random.choice(len(ia_l.samples))], W_ia[i])
                 # np.dot(ia_l.samples[np.random.choice(len(ia_l.samples))], self.Q), W_ia[i])
-                print("IASHAPE",IA_ef.shape)
-         
+                if average_all:
+                    IA_ef = np.reshape(IA_ef, newshape=(31,412))
+                    mean = np.mean(IA_ef, axis=0, keepdims=True)
+                    IA_ef = (1/31.0) * np.reshape(np.tile(mean, reps=(31,1)), (-1)) 
                 μ[i, :] = np.exp(IA_ef +
                             np.dot(T_S, W_t_s[i]) +
                             result_TT[i] + 
