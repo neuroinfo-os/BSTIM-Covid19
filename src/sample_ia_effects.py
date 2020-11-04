65fb038f1d75411684c75538b297d867c77fda09
Pascal <pnieters@uos.de>
2020-11-04 16:28:36 +0100

    Only sample IA Effects where necessary
    * cut off data after 25 days after train start for IA sampler
    * this should have 0 behaviour change for new data, but make
    resampling old days slightly faster!

--- src/sample_ia_effects.py
+++ src/sample_ia_effects.py
@@ -15,9 +15,9 @@ def main(start, task_id, num_weeks, csv_path):
     start_date = pd.Timestamp("2020-01-28") + pd.Timedelta(start, unit='d')
     disease = "covid19"
 
-    year = start_date.year
-    month = start_date.month
-    day = start_date.day
+    year = str(start_date)[:4]
+    month = str(start_date)[5:7]
+    day = str(start_date)[8:10]
     day_folder_path = "../data/ia_effect_samples/{}_{}_{}".format(year, month, day)
 
     Path(day_folder_path).mkdir(parents=True, exist_ok=True)
@@ -40,17 +40,23 @@ def main(start, task_id, num_weeks, csv_path):
 
 
     data = load_data_n_weeks(start, num_weeks, csv_path)
+    day_0 = start_date + pd.Timedelta(days=num_weeks*7+5)
+    _, target, _, _ = split_data(
+        data,
+        train_start=start_date,
+        test_start=day_0,
+        post_test=day_0)
 
     print("DaysTest")
-    print(data.index)
+    print(target.index)
 
     # RNGenerators
     rnd_tsel = np.random.Generator(np.random.PCG64())
-    times = uniform_times_by_day(data.index, rnd_tsel)
+    times = uniform_times_by_day(target.index, rnd_tsel)
     rnd_csel = np.random.Generator(np.random.PCG64())
     locs = uniform_locations_by_county(counties, rnd_csel)
 
-    res = iaeffect_sampler(data, times, locs, temporal_bfs, spatial_bfs)
+    res = iaeffect_sampler(target, times, locs, temporal_bfs, spatial_bfs)
     results = {"ia_effects": res, "predicted day": data.index,
                "predicted county": data.columns}
 
